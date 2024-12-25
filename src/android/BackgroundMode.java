5148f61fba16a7488003b1c240d3fa191c756151
ABDELKRIM ASSOUANE <66749387+karimaswan22@users.noreply.github.com>
2024-12-25 20:50:58 +0100

    Update BackgroundMode.java

--- src/android/BackgroundMode.java
+++ src/android/BackgroundMode.java
@@ -39,7 +39,7 @@ import static android.content.Context.BIND_AUTO_CREATE;
 import static de.appplant.cordova.plugin.background.BackgroundModeExt.clearKeyguardFlags;
 
 public class BackgroundMode extends CordovaPlugin {
- Log.e(TAG, "START code");
+ Log.e("heylogs", "START code");
 
 
 
